32530441d17ad8cb8c3b7ede6ae2c0ce976bf60a
Stefan Halapa <s.halapa@levi9.com>
2025-05-16 18:40:24 +0200

    Add tail logs, and read logs functions

--- scripts/functions.zsh
+++ scripts/functions.zsh
@@ -1,7 +1,3 @@
-s3() {
-  aws s3 ls --profile "$1"
-}
-
 s3explore() {
     local bucket
     bucket=$(aws s3 ls --profile dta | awk '{print $NF}' | fzf)
@@ -152,4 +148,54 @@ tpl() {
     local pipeline_id
     pipeline_id=$(pd "$pipeline_name" | jq -r '.[].actions[].id' | fzf)
     tailLogs "$pipeline_id"
+}
+
+#Tail logs for choosen log group
+tl() {
+    local log_group
+    log_group=$(aws logs describe-log-groups --query 'logGroups[*].logGroupName' --output text --profile dta | tr '\t' '\n' | fzf)
+    if [ -z "$log_group" ]; then
+        echo "No log group selected."
+        return 1
+    fi
+    aws logs tail "$log_group" --follow --profile dta
+}
+
+
+# read logs for choosen log group
+rl() {
+    local log_group
+    log_group=$(aws logs describe-log-groups --query 'logGroups[*].logGroupName' --output text --profile dta | tr '\t' '\n' | fzf)
+    if [ -z "$log_group" ]; then
+        echo "No log group selected."
+        return 1
+    fi
+    echo "Selected log group: $log_group"
+
+    local log_stream
+    log_stream=$(aws logs describe-log-streams --log-group-name "$log_group" --order-by LastEventTime --descending --query 'logStreams[*].logStreamName' --output text --profile dta | tr '\t' '\n' | fzf)
+    if [ -z "$log_stream" ]; then
+        echo "No log stream selected."
+        return 1
+    fi
+    echo "Selected log stream: $log_stream"
+
+    # Debugging timestamps
+    local start_time end_time
+    start_time=$(date -v-1d +%s000)
+    end_time=$(date +%s000)
+    echo "Start time: $start_time, End time: $end_time"
+
+    # Fetching logs with time range and query for messages, adding color for errors and warnings, and piping to less
+    aws logs get-log-events \
+        --log-group-name "$log_group" \
+        --log-stream-name "$log_stream" \
+        --query 'events[*].message' \
+        --output text \
+        --profile dta | awk '
+        /error/   { print "\033[1;31m" $0 "\033[0m"; next } # Red for errors
+        /ERROR/   { print "\033[1;31m" $0 "\033[0m"; next }
+        /warn/    { print "\033[1;33m" $0 "\033[0m"; next } # Orange for warnings
+        /WARN/    { print "\033[1;33m" $0 "\033[0m"; next }
+                    { print $0 }' | less -R
 }
